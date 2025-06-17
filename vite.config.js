0d0d04d95fa0b02e6fa296f9b4f38c38a46361c9
1zll <maho-s125@ezweb.ne.jp>
2025-06-18 05:04:48 +0900

    baseを/に

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-portfolio/',
+  base: '/',
   server: {
-    historyApiFallback: true, // ← これないとダイレクトアクセスで404になる
-  },
+    historyApiFallback: {
+      disableDotRule: true,
+    }
+  }  
 })
