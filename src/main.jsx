4fa1bc9adab03385592fd3df7e3fd42c3caf0abc
1zll <maho-s125@ezweb.ne.jp>
2025-06-18 02:27:50 +0900

    変更

--- src/main.jsx
+++ src/main.jsx
@@ -10,36 +10,33 @@ import ScrollToTop from "./components/ScrollToTop";
 import Start from "./pages/Start.jsx";
 import Project from "./pages/Project.jsx";
 
-// 就活用ページ（本番）
+// 就活用ページ
 import JobHome from "./pages/job/Home.jsx";
 import JobProfile from "./pages/job/Profile.jsx";
 
-// 公開用ページ（個人情報なし）
+// 公開用ページ
 import PublicHome from "./pages/public/Home.jsx";
 import PublicProfile from "./pages/public/Profile.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <BrowserRouter>
+      {/* GitHub Pagesで /my-portfolio 以下にデプロイするなら basenameを設定 */}
+      <BrowserRouter basename="/my-portfolio">
         <ScrollToTop />
         <Layout>
           <Routes>
-            {/* 公開用ルート */}
-            <Route path="/public/*">
-              <Route path="home" element={<PublicHome />} />
-              <Route path="profile" element={<PublicProfile />} />
-              <Route path="projects/:slug" element={<Project />} />
-            </Route>
+            {/* 公開用 */}
+            <Route path="/public/home" element={<PublicHome />} />
+            <Route path="/public/profile" element={<PublicProfile />} />
+            <Route path="/public/projects/:slug" element={<Project />} />
 
-            {/* 就活用ルート */}
-            <Route path="/job/*">
-              <Route path="home" element={<JobHome />} />
-              <Route path="profile" element={<JobProfile />} />
-              <Route path="projects/:slug" element={<Project />} />
-            </Route>
+            {/* 就活用 */}
+            <Route path="/job/home" element={<JobHome />} />
+            <Route path="/job/profile" element={<JobProfile />} />
+            <Route path="/job/projects/:slug" element={<Project />} />
 
-            {/* 共通ルート */}
+            {/* 共通 */}
             <Route path="/" element={<Start />} />
           </Routes>
         </Layout>
