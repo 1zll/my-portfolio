958c7d3954eb4e09e9d5968d748ea63770f12e52
1zll <maho-s125@ezweb.ne.jp>
2025-06-27 09:55:07 +0900

    404対策してみた

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === 'production' ? '/my-portfolio/' : '/',  server: {
+  base: mode === 'production' ? '/my-portfolio/' : '/',
+  server: {
     historyApiFallback: {
       disableDotRule: true,
     }
-  }  
-})
+  }
+}))
